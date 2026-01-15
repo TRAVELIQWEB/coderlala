5914f19f416756bb5e9c32dad0fe5d1fed64329d
ravi-kaliya <ravi.k@webshlok.com>
2026-01-15 15:45:42 +0530

    robots modify

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -4,7 +4,7 @@ export default function robots(): MetadataRoute.Robots {
   return {
     rules: {
       userAgent: "*",
-      allow: ["/", "/site.webmanifest", "/manifest.json", "/favicon.ico", "/favicon"],
+      allow: ["/",],
       // disallow: "/private/",
       // Example disallows if needed:
       disallow: ["/private"],
